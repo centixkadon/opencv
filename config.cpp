feef291b9241133bb5b3110ce4699876c535c038
centixkadon <centixkadon@gmail.com>
2018-02-14 23:10:31 +0800

    Update config

--- config.cpp
+++ config.cpp
@@ -1,12 +1,13 @@
 
 #include "precomp.hpp"
 
-// DEFINE_string(cv_images_root, "/home/eelab/All/datasets/dcbgrid/", "images root");
-// DEFINE_string(cv_images_group, "book/", "images group");
-// DEFINE_string(cv_images_prefix_left, "L", "left images prefix");
-// DEFINE_string(cv_images_prefix_right, "R", "right images prefix");
-// DEFINE_string(cv_images_prefix_groundtruth, "TR", "groundtruth images prefix");
-// DEFINE_string(cv_images_postfix, ".png", "images postfix");
+#include "config.hpp"
+
+using namespace std;
+using namespace google;
+
+DECLARE_string(flagfile);
+
 DEFINE_string(cv_images_root, "", "images root");
 DEFINE_string(cv_images_group, "", "images group");
 DEFINE_string(cv_images_prefix_left, "", "left images prefix");
@@ -14,15 +15,12 @@ DEFINE_string(cv_images_prefix_right, "", "right images prefix");
 DEFINE_string(cv_images_prefix_groundtruth, "", "groundtruth images prefix");
 DEFINE_string(cv_images_postfix, "", "images postfix");
 
-using namespace std;
-using namespace google;
-
 int config_init(int argc, char ** argv) {
-  SetCommandLineOption("flagfile", "ddrsgm_config.cfg");
+  SetCommandLineOption("flagfile", "config.cfg");
   ParseCommandLineFlags(&argc, &argv, true);
 
-  cout << "cvConfig: cv_images_root" << FLAGS_cv_images_root << endl;
-  cout << "cvConfig: cv_images_root" << FLAGS_cv_images_group << endl;
+  cout << "cvConfig: cv_images_root = " << FLAGS_cv_images_root << endl;
+  cout << "cvConfig: cv_images_group= " << FLAGS_cv_images_group << endl;
 
   return 0;
 }
