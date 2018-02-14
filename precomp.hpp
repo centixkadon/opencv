d92f4da7b8b481bffcc928273fd3999707758383
centixkadon <centixkadon@gmail.com>
2018-02-14 20:41:36 +0800

    Create precompiled header

---
+++ precomp.hpp
@@ -0,0 +1,34 @@
+
+#pragma once
+
+#include "qt.h"
+
+#include <algorithm>
+#include <functional>
+#include <numeric>
+
+#include <iterator>
+#include <memory>
+#include <utility>
+
+#include <array>
+#include <deque>
+#include <forward_list>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include <gflags/gflags.h>
+
+#include <opencv2/opencv.hpp>
+
+#include "config.hpp"
