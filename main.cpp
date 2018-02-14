70fefbf9c00b3be3907178f449f4c1ce54304624
centixkadon <centixkadon@gmail.com>
2018-02-14 20:41:54 +0800

    Create main

---
+++ main.cpp
@@ -0,0 +1,13 @@
+
+#include "precomp.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char ** argv) {
+  cout << "cvMain: Hello OpenCV" << endl;
+
+  config_init(argc, argv);
+
+  return 0;
+}
