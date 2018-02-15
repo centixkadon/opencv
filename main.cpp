4f0bd8549532fdada351dd5a6531ba55e6d4e9b3
centixkadon <centixkadon@gmail.com>
2018-02-16 00:47:52 +0800

    Update main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 
 #include "precomp.hpp"
 
+#include "config.hpp"
+#include "images.hpp"
+
 using namespace std;
 using namespace cv;
 
@@ -8,6 +11,13 @@ int main(int argc, char ** argv) {
   cout << "cvMain: Hello OpenCV" << endl;
 
   config_init(argc, argv);
+
+  thread_list<ImageFrame> images_pipe1, images_pipe2;
+  thread th_images_read(images_read, ref(images_pipe1));
+  thread th_images_process(images_process, ref(images_pipe2), ref(images_pipe1));
+
+  th_images_read.join();
+  th_images_process.join();
 
   return 0;
 }
