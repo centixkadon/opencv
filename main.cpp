77a03747073af1fb7906c6049aa95c8944d223ca
centixkadon <centixkadon@gmail.com>
2018-02-16 19:32:46 +0800

    Update main

--- main.cpp
+++ main.cpp
@@ -12,12 +12,15 @@ int main(int argc, char ** argv) {
 
   config_init(argc, argv);
 
-  thread_list<ImageFrame> images_pipe1, images_pipe2;
-  thread th_images_read(images_read, ref(images_pipe1));
-  thread th_images_process(images_process, ref(images_pipe2), ref(images_pipe1));
+  thread_list<ImageRead> image_reads;
+  thread_list<ImageWrite> images_writes;
+  thread th_images_read(images_read, ref(image_reads));
+  thread th_images_process(images_process, ref(images_writes), ref(image_reads));
+  thread th_images_write(images_write, ref(images_writes));
 
   th_images_read.join();
   th_images_process.join();
+  th_images_write.join();
 
   return 0;
 }
